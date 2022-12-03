2da88e2849b5e0842a388a4c003d8ddf5ac9dc1c
Meacha1 <meachattd@gmail.com>
2022-12-03 16:36:27 +0000

    commit message

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,3 @@ def no_c(my_string):
     if my_string:
         my_string = my_string.translate({ord(i): None for i in 'Cc'})
         return (my_string)
-    if my_string == None:
-        return (None)
