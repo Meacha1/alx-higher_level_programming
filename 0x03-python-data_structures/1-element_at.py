19bbfa5be1fc2adbaf675e7fd6d7cc5bc04aef81
Meacha1 <meachattd@gmail.com>
2022-12-03 15:12:46 +0000

    commit message

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,5 +5,5 @@ def element_at(my_list, idx):
             return(my_list[i])
     if idx < 0:
         return (None)
-    if idx > len(my_list):
+    if idx > (len(my_list) - 1):
         return (None)
