60c52f816ac25bbd6c83b7c71b46453c840f2bed
Meacha1 <meachattd@gmail.com>
2022-12-04 09:12:13 +0000

    commit message

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,5 @@ def print_matrix_integer(matrix=[[]]):
             if j != (len(matrix[i]) - 1):
                 print("{:d}{}".format(matrix[i][j], " "), end="")
             else:
-                print("{:d}".format(matrix[i][j]), end="$\n")
+                print("{:d}".format(matrix[i][j]), end="$")
+        print()
