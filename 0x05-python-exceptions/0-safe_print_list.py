4dd67bbbb0dc49efeea5b170230618e1f8e4c0c2
Meacha1 <meachattd@gmail.com>
2022-12-19 20:41:25 +0000

    commit message

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
+    try:
+        count = 0
+        for i in range(0, x):
+            if i+1 == x:
+                print("{:d}".format(my_list[i]))
+                count += 1
+            else:
+                print("{:d}".format(my_list[i]), end = "")
+                count += 1
+    except:
+        print()
+    return count
