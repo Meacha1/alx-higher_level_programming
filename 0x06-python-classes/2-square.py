29a30ee58cbf1c23a155f0dd015cf26e642b1c23
Meacha1 <meachattd@gmail.com>
2022-12-20 17:35:49 +0000

    commit message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,4 +10,8 @@ class Square:
 
     def __init__(self, size=0):
         """intialization of the vars"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
