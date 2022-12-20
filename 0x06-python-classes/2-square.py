07737fa60ffefe7a68aa844d96d7a506fd921ffa
Meacha1 <meachattd@gmail.com>
2022-12-20 17:27:14 +0000

    commit message

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (with int type/value verification).
+    """
+
+    def __init__(self, size=0):
+    """intialization of the vars"""
+        self.__size = size
