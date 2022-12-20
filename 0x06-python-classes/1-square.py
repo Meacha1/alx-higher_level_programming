295ff55a07b367a342fcb2f710b58360810382ff
Meacha1 <meachattd@gmail.com>
2022-12-20 17:09:09 +0000

    commit message

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,7 +7,7 @@ class Square:
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
-    
+
     def __init__(self, size):
         """Initializes the var"""
         self.__size = size
