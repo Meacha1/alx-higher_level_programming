afb4edaa70b99347452784ff58aa0f8100f26310
Meacha1 <meachattd@gmail.com>
2023-01-09 20:31:55 +0000

    commit message

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Contains the inherits_from function
 Returns True if the object is an instance of a class that inherited (directly or indirectly)
 from the specified class; otherwise False.
 
