8201f733412d80201f991595e31f56cc2e46d6fa
Meacha1 <meachattd@gmail.com>
2023-01-09 20:28:20 +0000

    commit message

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,7 @@ bool: True if the object is an instance of a class that inherited from the speci
       False otherwise.
 """
 
+
 def inherits_from(obj, a_class):
     """returns true if obj is a subclass of a_class, otherwise false"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
