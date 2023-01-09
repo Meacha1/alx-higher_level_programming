6c23219ddd5153bf6a9663188a86c087c45aee26
Meacha1 <meachattd@gmail.com>
2023-01-09 19:44:52 +0000

    commit message

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ contains the MyList class
 
 
 class MyList(list):
-"""subclass called MyList"""
+    """subclass called MyList"""
     def print_sorted(self):
     """prints a sorted List"""
         self.sort()
