5a2790293d09737221f29810887d76d1e72f909d
Meacha1 <meachattd@gmail.com>
2023-01-09 20:27:15 +0000

    commit message

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Returns True if the object is an instance of a class that inherited (directly or indirectly)
+from the specified class; otherwise False.
+
+Parameters:
+obj (object): The object to check.
+a_class (class): The class to check against.
+
+Returns:
+bool: True if the object is an instance of a class that inherited from the specified class;
+      False otherwise.
+"""
+
+def inherits_from(obj, a_class):
+    """returns true if obj is a subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
