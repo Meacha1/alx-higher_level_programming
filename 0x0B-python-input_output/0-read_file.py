4e598f86e71bc6b10f3566184e72ba8e0d7fb3ab
Meacha1 <meachattd@gmail.com>
2023-01-10 15:18:06 +0000

    commit message

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+function that reads a text file
+"""
+
+
 def read_file(filename=""):
+    """reads file with a file discripter"""
     with open(filename, 'r', encoding='utf8') as file:
+    """reads a file a print out to stdout"""
         for line in file:
             print(line)
