26138afeaeb8911e55c7b597aef542db08dcec0e
Meacha1 <meachattd@gmail.com>
2023-01-10 15:13:18 +0000

    commit message

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 def read_file(filename=""):
-
-    with open('UTF8', 'r') as f:
-        print(f.read())
+    with open(filename, 'r', encoding='utf8') as file:
+        for line in file:
+            print(line)
