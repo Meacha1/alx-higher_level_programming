9d260428944f9ae7b47c1ce39587daf53f072000
Meacha1 <meachattd@gmail.com>
2023-01-10 15:21:44 +0000

    commit message

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,5 @@ function that reads a text file
 def read_file(filename=""):
     """reads file with a file discripter"""
     with open(filename, 'r', encoding='utf8') as file:
-    """reads a file a print out to stdout"""
-        for line in file:
-            print(line)
+        r_file = file.read()
+        print(r_file, end='')
