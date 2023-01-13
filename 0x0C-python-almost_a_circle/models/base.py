da4f6dcae43d1e2ef11c399e0051bb789d089117
Meacha1 <meachattd@gmail.com>
2023-01-13 11:27:57 +0000

    commit message

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+class Base:
+
+    __nb_objects = 0
+    def __init__(slef, id=None):
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = id
