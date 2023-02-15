f71f2c1d5bb02877fde3657b7fe815aeb1ce76b1
Meacha1 <meachattd@gmail.com>
2023-02-15 10:06:18 -0800

    create

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- Creates a table with id and name
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL;
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
