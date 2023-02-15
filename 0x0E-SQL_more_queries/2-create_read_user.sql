96bf51a35996018ccff232434e09205e56807378
Meacha1 <meachattd@gmail.com>
2023-02-15 09:49:11 -0800

    creating_db_

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creates DB and user with a limited privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON database TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
