9a903b6f8ed15cd95a3f5ed9b756f484a7ce2f85
Meacha1 <meachattd@gmail.com>
2023-03-13 11:38:40 -0700

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 const count = process.argv.length;
 
-if (count == 2)
-{
-	console.log('No argument');
+if (count === 2) {
+  console.log('No argument');
 }
-if (count == 3)
-{
-	console.log('Argument found');
+if (count === 3) {
+  console.log('Argument found');
 }
-if (count > 3)
-{
-	console.log('Arguments found');
+if (count > 3) {
+  console.log('Arguments found');
 }
