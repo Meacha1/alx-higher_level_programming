3235c96928819f2106acf89e1b0ec0c7bcd5d917
Meacha1 <meachattd@gmail.com>
2023-03-13 12:42:08 -0700

    sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const count = process.argv.length;
+
+if (count === 3) {
+  console.log(process.argv[2] + ' is undefined');
+}
+if (count === 2) {
+  console.log('undefined is undefined');
+}
+if (count === 4) {
+  console.log(process.argv[2] + ' is ' + process.argv[3])
+}
