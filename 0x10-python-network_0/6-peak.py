523e7046371b8558aec3c6d7a25c63d4148ac88b
Meacha1 <meachattd@gmail.com>
2023-03-30 12:38:44 +0000

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+def find_peak(list_of_integers):
+"""Finds a peak in a list of unsorted integers."""
+
+    # Initialize the start and end indices.
+    start = 0
+    end = len(list_of_integers) - 1
+
+    # Binary search for a peak.
+    while start < end:
+        mid = (start + end) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            start = mid + 1
+        else:
+            end = mid
+
+    return list_of_integers[start]
