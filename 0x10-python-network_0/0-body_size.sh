2751c87e53123b122bc67cc7300e6661a1ac6c34
Meacha1 <meachattd@gmail.com>
2023-03-30 03:07:23 -0700

    curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Take URL as argument
-url=$1
-
-# Send GET request using curl and store the response body in a variable
-response=$(curl -sS "$url")
-
-# Get the size of the response body in bytes using the "wc" command and display it
-echo -n "$response" | wc -c
+url=$1 | response=$(curl -sS "$url") | echo -n "$response" | wc -c
