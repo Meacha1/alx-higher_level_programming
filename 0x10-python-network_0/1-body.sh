1f468999c2e10a6c916c14b286ec448ddd19d2c7
Meacha1 <meachattd@gmail.com>
2023-03-30 03:38:34 -0700

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a GET request to a URL and display the response body
-curl -s -o /dev/null -w "%{http_code}\n" $1 | grep -q 200 && curl -s $1
+# Script to send a GET request to a URL and display the body
+curl -sL "$1"
