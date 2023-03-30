c61c788e63291a989b3f317b2b0c08fd477e0b30
Meacha1 <meachattd@gmail.com>
2023-03-30 13:15:16 +0000

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,7 @@ Finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    """Finds a peak in a list of integers"""
     if not list_of_integers:
     return None
     # Initialize the start and end indices.
