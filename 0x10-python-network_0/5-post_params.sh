5a2c5d268673c6f65a113a8878f82bafa911fae3
Meacha1 <meachattd@gmail.com>
2023-03-30 11:37:36 +0000

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
-s script takes in a URL as an argument, sends a GET request to the URL
+#!/bin/bash
+# This script takes in a URL as an argument, sends a GET request to the URL
 curl -sd "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
