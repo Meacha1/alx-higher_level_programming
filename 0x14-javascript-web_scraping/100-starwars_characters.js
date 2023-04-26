c4c88a5a8138d85a1d75c4632db8b20843989c97
Meacha1 <meachattd@gmail.com>
2023-04-26 14:44:55 -0700

    chars

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const movieId = process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  const json = JSON.parse(body);
+  const characters = json.characters;
+  for (const character of characters) {
+    request(character, function (error, response, body) {
+      if (error) {
+        return console.log(error);
+      }
+      const json = JSON.parse(body);
+      console.log(json.name);
+    });
+  }
+});
