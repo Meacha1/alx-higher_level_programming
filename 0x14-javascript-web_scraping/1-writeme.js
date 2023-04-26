7c833431e3c35182123baa949b9b84cd8c5a5287
Meacha1 <meachattd@gmail.com>
2023-04-26 01:59:51 -0700

    write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const MyString = process.argv[2];
-const Myfile = process.argv[3];
+const MyString = process.argv[3];
+const Myfile = process.argv[2];
 const fs = require('fs');
 fs.writeFile(Myfile, MyString, 'utf-8', function (err) {
   if (err) {
