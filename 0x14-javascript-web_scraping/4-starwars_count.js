6d2e468f63a8e434f933ffe569e78020e03cb947
Meacha1 <meachattd@gmail.com>
2023-04-26 10:16:59 -0700

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+const wedgeAntillesId = '18';
 request(url, function (error, response, body) {
   if (error) {
     return console.log(error);
@@ -9,7 +10,7 @@ request(url, function (error, response, body) {
   let count = 0;
   for (let i = 0; i < json.results.length; i++) {
     for (let j = 0; j < json.results[i].characters.length; j++) {
-      if (json.results[i].characters[j].includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      if (json.results[i].characters[j].includes('https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/')) {
         count++;
       }
     }
