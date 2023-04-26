734f17b002c3881707e0302c0ba5c1790ef08a79
Meacha1 <meachattd@gmail.com>
2023-04-26 12:23:39 -0700

    store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,12 +4,12 @@ const file = process.argv[3];
 const request = require('request');
 const fs = require('fs');
 request(url, function (error, response, body) {
-    if (error) {
-        return console.error(error);
+  if (error) {
+    return console.error(error);
+  }
+  fs.writeFile(file, body, 'utf8', function (err) {
+    if (err) {
+      return console.error(err);
     }
-    fs.writeFile(file, body, 'utf8', function (err) {
-        if (err) {
-            return console.error(err);
-        }
-    });
+  });
 });
